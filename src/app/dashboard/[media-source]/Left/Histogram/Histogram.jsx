a35dd5d1f833e32c1eea491ad255efb2b1527a2d
JC CorMan <cordovez@el-cordovez.com>
2023-06-15 21:33:07 +0200

    Text analysis for article received

--- src/app/dashboard/[media-source]/Left/Histogram/Histogram.jsx
+++ src/app/dashboard/[media-source]/Left/Histogram/Histogram.jsx
@@ -1,4 +1,23 @@
 export default function Histogram({ results }) {
+  if (!results) {
+    return (
+      <div className="bg-base-200 mb-12 p-12 max-w-lg">
+        <p>No data</p>
+      </div>
+    );
+  }
+  console.log(results);
+  const sentences = results.sentences;
+  const verb_count = results.pos.verb_count;
+  const adverb_count = results.pos.adverb_count;
+  const adjective_count = results.pos.adjective_count;
+  // console.log(
+  //   sentences.total_sentences,
+  //   verb_count,
+  //   adverb_count,
+  //   adjective_count
+  // );
+
   return (
     <>
       <div
@@ -10,7 +29,18 @@ export default function Histogram({ results }) {
         // }}
       >
         <div className="prose">
-          <h1>Histogram</h1> <div>{results}</div>
+          <h1>Histogram</h1>
+          {/* <pre>{JSON.stringify(results)}</pre> */}
+          <p>There are {sentences.total_sentences} sentences in this article</p>
+          <p>{adjective_count} adjectives</p>
+          <p>{adverb_count} adverbs</p>
+          <p>{verb_count} verbs</p>
+          <h3>And here are the potential phrasal verbs:</h3>
+          <ul className="list-disc">
+            {results.phrasal_verbs.map((phrase) => (
+              <li key={phrase}>{phrase}</li>
+            ))}{" "}
+          </ul>
         </div>
       </div>
     </>
